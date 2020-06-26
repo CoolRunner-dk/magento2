2866d53d191217ebbc171c3e33726c7cb18da41c
kevinhansen <kh@coorlunner.dk>
2020-06-26 14:54:19 +0200

    update

--- CoolRunner/Shipping/Block/Adminhtml/Order/View/CustomOrderView.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace CoolRunner\Shipping\Block\Adminhtml\Order\View;
-
-class CustomOrderView extends \Magento\Backend\Block\Template
-{
-
-}
--- CoolRunner/Shipping/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'CoolRunner_Shipping',
-    __DIR__
-);
-
